6a4b978cea3bdcff2beca93d908e15c155c8d99c
Amy Boudsady <aeimskei@gmail.com>
2018-04-20 06:54:04 -0700

    Add error handling for 500 and 404

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 
+// Error handling
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({error: err})
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({error: {message: `Not found`}})
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
