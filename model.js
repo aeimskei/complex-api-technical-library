722c145ce7777a0834e1579d7a5dde89bc40f489
Amy Boudsady <aeimskei@gmail.com>
2018-04-21 12:40:57 -0700

    Setup GET for books

--- model.js
+++ model.js
@@ -3,3 +3,22 @@ const ids = require('short-id')
 const path = './books.json'
 
 const books = JSON.parse(fs.readFileSync(path))
+
+// ===========================================
+// GET, Read books data
+// ===========================================
+
+function getAllBooks() {
+  return books;
+}
+
+function getBookById(id) {
+  const book = books.find(book => book.id === id)
+
+  if (!book) {
+    return {error: 404, message: `Sorry, book with id ${id} was not found.`}
+  } else {
+    return book;
+  }
+}
+
